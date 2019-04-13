bbbec07f8557869fef9842a967663db87191cc61
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-04-13 02:33:52 -0400

    created a word cell and set the function to size the cell

--- twitter/HomeController.swift
+++ twitter/HomeController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class HomeController: UICollectionViewController {
+class WordCell: UICollectionViewCell {
+    
+    //this gets called when a cell is dequeued
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     let cellId = "cellId"
     
     override func viewDidLoad() {
-        collectionView.backgroundColor = .green
+        collectionView.backgroundColor = .white
+        collectionView.register(WordCell.self, forCellWithReuseIdentifier: cellId)
     }
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 4
     }
@@ -22,5 +37,8 @@ class HomeController: UICollectionViewController {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
         cell.backgroundColor = .blue
         return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
     }
 }
