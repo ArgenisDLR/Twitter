c56adcaa797741ab72a6f85ec17372441dff3e3f
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-04-13 03:13:06 -0400

    customized the label and pinned to the parent view. Connected the code to viewdidload and also updated the header and footer

--- twitter/HomeController.swift
+++ twitter/HomeController.swift
@@ -13,7 +13,26 @@ class WordCell: UICollectionViewCell {
     //this gets called when a cell is dequeued
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupViews()
+    }
+    //customizing the Label in the View
+    let wordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "test test test"
+        label.translatesAutoresizingMaskIntoConstraints = true
+        return label
+    }()
+    
+    func setupViews() {
         backgroundColor = .yellow
+        
+        addSubview(wordLabel)
+        
+        //pinning the layout to the parent view through top anchor
+        wordLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        wordLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        wordLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        wordLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -23,13 +42,20 @@ class WordCell: UICollectionViewCell {
 
 class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
+    //constant identifiers
     let cellId = "cellId"
+    let headerId = "headerId"
+    let footerId = "footerId"
     
+    //loadup screen
     override func viewDidLoad() {
         collectionView.backgroundColor = .white
         collectionView.register(WordCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
+        collectionView.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: footerId)
     }
     
+    //collectionView layout by cells, number of items, and appearance
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 4
     }
@@ -41,4 +67,28 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: 50)
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        if kind == UICollectionView.elementKindSectionHeader {
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
+            header.backgroundColor = .blue
+            return header
+        } else {
+            let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: footerId, for: indexPath)
+            footer.backgroundColor = .blue
+            return footer
+        }
+        
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+    
 }
