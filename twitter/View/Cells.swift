f55e3e73e6299588e2acff535b71d87351fd33f6
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-04-14 07:01:25 -0400

    created cells for user

---
+++ twitter/View/Cells.swift
@@ -0,0 +1,87 @@
+//
+//  Cells.swift
+//  twitter
+//
+//  Created by argenis delarosa on 4/14/19.
+//  Copyright © 2019 argenis delarosa. All rights reserved.
+//
+
+import LBTAComponents
+
+class UserFooter: DatasourceCell {
+    override func setupViews() {
+        super.setupViews()
+        backgroundColor = .green
+    }
+}
+
+class UserHeader: DatasourceCell {
+    override func setupViews() {
+        super.setupViews()
+        backgroundColor = .blue
+    }
+}
+
+class UserCell: DatasourceCell {
+    
+    override var datasourceItem: Any? {
+        didSet {
+//            nameLabel.text = datasourceItem as? String
+        }
+    }
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "HENNYGAWD")
+        imageView.layer.cornerRadius = 5
+        imageView.layer.masksToBounds = true
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Argenis De La Rosa"
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "@hennyrosexo"
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    let bioTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .yellow
+        return textView
+    }()
+    
+    let followButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .cyan
+        return button
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        addSubview(profileImageView)
+        addSubview(nameLabel)
+        addSubview(usernameLabel)
+        addSubview(bioTextView)
+        addSubview(followButton)
+        
+        profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+
+        nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
+        
+        usernameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+        
+        bioTextView.anchor(usernameLabel.bottomAnchor, left: usernameLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+        followButton.anchor(topAnchor, left: nil, bottom: nil, right: self.rightAnchor, topConstant: 12, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 120, heightConstant: 34)
+    }
+}
